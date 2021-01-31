82610f1c3db804d78e0dc2c297a8c4911690a0b6
joshuainchilliwack <33652260+joshuainchilliwack@users.noreply.github.com>
2021-01-30 22:30:53 -0800

    add, update, delete student using jdbc

--- student/src/com/luv2code/jsf/hello/Student.java
+++ student/src/com/luv2code/jsf/hello/Student.java
@@ -1,75 +1,31 @@
 package com.luv2code.jsf.hello;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.component.UIComponent;
-import javax.faces.context.FacesContext;
-import javax.faces.validator.ValidatorException;
 
 @ManagedBean
 public class Student {
 
+	private int id;
 	private String firstName;
 	private String lastName;
 	private String email;
-	private String country;
-	private String gender;
-	private String postalCode;
-	private String phoneNumber;
-	private int freePasses;
-	private String courseCode;
-
-	private String[] favoriteLanguages;
-
-	List<String> countryOptions;
-
+	
 	public Student() {
-		countryOptions = new ArrayList<>();
-
-		countryOptions.add("Brazil");
-		countryOptions.add("France");
-		countryOptions.add("Germany");
-		countryOptions.add("India");
-		countryOptions.add("Turkey");
-		countryOptions.add("United Kingdom");
-		countryOptions.add("United States");
 	}
-
-	public Student(String firstName, String lastName, String email) {
+	
+	public Student(int id, String firstName, String lastName, String email) {
+		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
 	}
 
-	public String getEmail() {
-		return email;
+	public int getId() {
+		return id;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String[] getFavoriteLanguages() {
-		return favoriteLanguages;
-	}
-
-	public void setFavoriteLanguages(String[] favoriteLanguages) {
-		this.favoriteLanguages = favoriteLanguages;
-	}
-
-	public String getGender() {
-		return gender;
-	}
-
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-
-	public List<String> getCountryOptions() {
-		return countryOptions;
+	public void setId(int id) {
+		this.id = id;
 	}
 
 	public String getFirstName() {
@@ -88,61 +44,18 @@ public class Student {
 		this.lastName = lastName;
 	}
 
-	public String getCountry() {
-		return country;
+	public String getEmail() {
+		return email;
 	}
 
-	public void setCountry(String country) {
-		this.country = country;
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
-	public int getFreePasses() {
-		return freePasses;
-	}
-
-	public void setFreePasses(int freePasses) {
-		this.freePasses = freePasses;
-	}
-
-	public String getPostalCode() {
-		return postalCode;
-	}
-
-	public void setPostalCode(String postalCode) {
-		this.postalCode = postalCode;
-	}
-
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-
-	public String getCourseCode() {
-		return courseCode;
-	}
-
-	public void setCourseCode(String courseCode) {
-		this.courseCode = courseCode;
-	}
-
-	public void validateCourseCode(FacesContext context, UIComponent component, Object value)
-			throws ValidatorException {
-
-		if (value == null) {
-			return;
-		}
-
-		String data = value.toString();
-
-		if (!data.startsWith("CMPT")) {
-
-			FacesMessage message = new FacesMessage("Course code must start with CMPT");
-
-			throw new ValidatorException(message);
-		}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", firstName=" + firstName + ", lastName="
+				+ lastName + ", email=" + email + "]";
 	}
 
 }
